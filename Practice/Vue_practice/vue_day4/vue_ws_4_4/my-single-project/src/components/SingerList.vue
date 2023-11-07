9d77ce129d36e76b5bdb673d76d48d4e2220f28e
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-07 15:57:05 +0900

    vue_ws_4_4

---
+++ Practice/Vue_practice/vue_day4/vue_ws_4_4/my-single-project/src/components/SingerList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="singer-list">
+    <h1 >가수 목록</h1>
+    <SingerDetail />
+    <SingerDetail />
+  </div>
+</template>
+
+<script setup>
+import SingerDetail from './singers/SingerDetail.vue';
+</script>
+
+<style scoped>
+.singer-list {
+  text-align: center;
+  padding: 10px;
+}
+
+.singer-list h1 {
+  color: orangered;
+}
+</style>
---
+++ Practice/Vue_practice/vue_day4/vue_ws_4_4/my-single-project/src/components/singers/SingerDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="singer-detail card">
+    <h1>가수 이름</h1>
+    <img src="@/assets/singer.jpg" alt="singer" style="width: 200px; border-radius: 50%;">
+    <p>가수 소개글</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.singer-detail {
+  text-align: center;
+  padding: 10px;
+  border: 1px solid #000;
+}
+
+.card {
+  display: inline-block;
+  width: 40%;
+  margin: 20px;
+}
+</style>
