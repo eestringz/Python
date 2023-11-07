6bd2ef6c177095f48f4c6c75672c9672174a75be
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-07 16:39:05 +0900

    vue_hw_4_4

---
+++ Practice/Vue_practice/vue_day4/vue_hw_4_4/my-first-front-pjt/src/components/ColorChanger.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-model="colorClass" >
+    <p :class="colorClass">올바른 색상 명을 입력하면 글자색이 바껴요</p>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue"
+  const colorClass = ref('')
+  
+</script>
+
+<style scoped>
+  .red{
+    color: red;
+  }
+
+  .blue{
+    color:blue;
+  }
+
+  .green{
+    color: green;
+  }
+  .default{
+    color: black;
+  }
+</style>
