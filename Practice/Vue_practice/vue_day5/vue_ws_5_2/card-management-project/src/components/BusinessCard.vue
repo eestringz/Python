572a3cbf6ba736b36efbf4b6e2737484ff036637
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-08 17:25:39 +0900

    vue_ws_5_2

---
+++ Practice/Vue_practice/vue_day5/vue_ws_5_2/card-management-project/src/components/BusinessCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="business-card-list">
+    <h2>보유 명함 목록</h2>
+    <div class="cards-container">
+      <businessCardDetail 
+        v-for="card in businessCards" 
+        :key="card.name"
+        :business-card="card" 
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import businessCardDetail from '@/components/businessCardDetail.vue';
+import {ref} from 'vue'
+
+const businessCards = ref([
+  {name : '일론머스크', title : '테슬라 테크노킹'},
+  {name : '래리 엘리슨', title : '오라클 창업주'},
+  {name : '빌 게이츠', title : '마이크로소프트 공동창업주'},
+  {name : '래리 페이지', title : '구글 공동창업주'},
+  {name : '세르게이 브린', title : '구글 공동창업주'},
+])
+
+
+
+</script>
+
+<style scoped>
+.business-card-list {
+  text-align: center;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+}
+</style>
---
+++ Practice/Vue_practice/vue_day5/vue_ws_5_2/card-management-project/src/components/businessCardDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="business-card-detail">
+    <h3>이름 : {{ businessCard.name }}</h3>
+    <p>직함 : {{ businessCard.title }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  businessCard : Object
+})
+
+</script>
+
+<style scoped>
+.business-card-detail {
+  background-color: #f9f9f9;
+  width: 40%;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+
+</style>
