86b929cc05436a6773b4a04de8193a8dcb2be9dc
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-08 17:26:14 +0900

    vue_ws_5_4

---
+++ Practice/Vue_practice/vue_day5/vue_ws_5_4/card-management-project/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app">
+    <header>
+      <h1>명함 관리 페이지</h1>
+    </header>
+    <main>
+      <p>명함을 관리하는 페이지입니다. 여기에 명함 목록이 표시됩니다.</p>
+      <article>
+        <BusinessCard />
+      </article>
+    </main>
+    <footer>
+      <p>&copy; 2023 My Business Cards</p>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import BusinessCard from '@/components/BusinessCard.vue';
+
+
+</script>
+
+<style scoped>
+.app {
+  text-align: center;
+  /* min-height: 100vh; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+header {
+  padding: 20px;
+  background-color: #007acc;
+  color: white;
+}
+
+main {
+  padding: 20px;
+}
+
+article {
+  flex-grow: 1;
+  padding: 20px;
+}
+
+footer {
+  background-color: #f0f0f0;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  padding: px;
+}
+</style>
---
+++ Practice/Vue_practice/vue_day5/vue_ws_5_4/card-management-project/src/components/BusinessCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="business-card-list">
+    <h2>보유 명함 목록</h2>
+    <p v-if="businessCards.length > 0">현재 보유중인 명함 수 : {{ businessCards.length }}</p>
+    <p v-else>명함이 없습니다. 새로운 명함을 추가해 주세요.</p>
+    <div class="cards-container">
+      <businessCardDetail 
+        v-for="card in businessCards" 
+        :key="card.name"
+        :business-card="card"
+        @delete-card-event="deleteCard" 
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import businessCardDetail from '@/components/businessCardDetail.vue';
+import {ref} from 'vue'
+
+const businessCards = ref([
+  {name : '일론머스크', title : '테슬라 테크노킹'},
+  {name : '래리 엘리슨', title : '오라클 창업주'},
+  {name : '빌 게이츠', title : '마이크로소프트 공동창업주'},
+  {name : '래리 페이지', title : '구글 공동창업주'},
+  {name : '세르게이 브린', title : '구글 공동창업주'},
+])
+
+const deleteCard = function (targetCard) {
+  businessCards.value = businessCards.value.filter((card) => card != targetCard)
+}
+
+</script>
+
+<style scoped>
+.business-card-list {
+  text-align: center;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+}
+</style>
