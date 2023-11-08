3dd23f1f25e1d7175379b603683252bb85e72aaa
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-08 17:20:39 +0900

    vue_ws_5_1

---
+++ Practice/Vue_practice/vue_day5/vue_hw_5_2/vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <ParentPage />
+  </div>
+</template>
+
+<script setup>
+import ParentPage from '@/components/ParentPage.vue';
+</script>
+
+<style scoped>
+.app {
+  border: 1px solid #333;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
---
+++ Practice/Vue_practice/vue_day5/vue_hw_5_2/vue-project/src/components/ParentPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="parent-page">
+    <h1>부모 페이지입니다.</h1>
+    <ChildernPage 
+      v-for="child in children" 
+      :key="child.name" 
+      :child="child"
+    />
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import ChildernPage from '@/components/ChildrenPage.vue'
+
+
+const children = ref([
+  {
+    name : '김하나',
+    age : 30
+  },
+  {
+    name : '김두리',
+    age : 20
+  },
+  {
+    name : '김서이',
+    age : 10
+  },
+])
+
+</script>
+
+<style scoped>
+.parent-page {
+  border: 1px solid #333;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
