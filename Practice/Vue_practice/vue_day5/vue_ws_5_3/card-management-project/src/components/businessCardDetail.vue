31f6254cefb999d1177a782c73c8b03f21cea886
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-08 17:25:53 +0900

    vue_ws_5_3

---
+++ Practice/Vue_practice/vue_day5/vue_ws_5_3/card-management-project/src/components/businessCardDetail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="business-card-detail">
+    <h3>이름 : {{ businessCard.name }}</h3>
+    <p>직함 : {{ businessCard.title }}</p>
+    <button @click="deleteCardEvent(businessCard)">명함 삭제</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  businessCard : Object
+})
+
+const emit = defineEmits(['deleteCardEvent'])
+
+const deleteCardEvent = function (targetCard) {
+  emit('deleteCardEvent', targetCard)
+}
+
+</script>
+
+<style scoped>
+.business-card-detail {
+  background-color: #f9f9f9;
+  width: 40%;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+
+</style>
