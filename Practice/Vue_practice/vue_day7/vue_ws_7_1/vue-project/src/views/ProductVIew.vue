f1adb09996bb6c09689257cced36c1d3c1e2e1a8
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-13 17:43:04 +0900

    vue_ws_7_1

---
+++ Practice/Vue_practice/vue_day7/vue_ws_7_1/vue-project/src/views/ProductVIew.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="products">
+    <h1 style="font-weight: 600;">상품 목록 페이지</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.products {
+  margin: 20px;
+  font-weight: 600;
+}
+
+
+</style>
