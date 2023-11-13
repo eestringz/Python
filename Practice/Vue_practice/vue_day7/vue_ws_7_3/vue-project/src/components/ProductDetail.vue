3e7421bcd0f761d700eaa8d1e98a4fddc24d3326
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-13 17:43:43 +0900

    vue_ws_7_3

---
+++ Practice/Vue_practice/vue_day7/vue_ws_7_3/vue-project/src/components/ProductDetail.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="product-card">
+    <p class="product-name">{{ product.name }}</p>
+    <img 
+      :src="product.imagePath"
+      :alt="product.name"
+      width="250"
+    >
+    <p class="product-price">₩ {{ product.price }}</p>
+    <span @click="store.updateProduct(product.name)">
+      <span v-if="product.isFavorite" class="favorite-icon" role="img" style="color:red">❤</span>
+      <span v-else class="favorite-icon" role="img">🤍</span>
+    </span>
+</div>
+</template>
+
+<script setup>
+import { useProductStore } from '@/stores/product';
+
+defineProps({
+  product : Object,
+})
+
+const store = useProductStore()
+
+</script>
+
+<style scoped>
+.product-card {
+  border: 1px solid #ccc;
+  text-align: center;
+  width: 350px;
+  padding: 20px;
+  margin: 10px;
+}
+
+.product-name {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+
+.product-price {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.favorite-icon {
+  cursor: pointer;
+  font-size: 20px;
+  margin-top: 10px;
+}
+</style>
---
+++ Practice/Vue_practice/vue_day7/vue_ws_7_3/vue-project/src/stores/product.js
@@ -0,0 +1,50 @@
+import { ref, computed } from 'vue'
+import { defineStore  } from 'pinia'
+
+export const useProductStore = defineStore('product', () => {
+  // states
+  const productList = ref([
+    {
+      name: '상품 1',
+      imagePath: 'src/assets/product1.png',
+      price: 10000,
+      isFavorite: false
+    },
+    {
+      name: '상품 2',
+      imagePath: 'src/assets/product2.png',
+      price: 20000,
+      isFavorite: false
+    },
+    {
+      name: '상품 3',
+      imagePath: 'src/assets/product3.png',
+      price: 30000,
+      isFavorite: false
+    },
+    {
+      name: '상품 4',
+      imagePath: 'src/assets/product4.png',
+      price: 40000,
+      isFavorite: false
+    }
+  ])
+
+  // actions
+  const updateProduct = function (name) {
+    productList.value = productList.value.map((product) => {
+      if (product.name === name ) {
+        product.isFavorite = !product.isFavorite
+      }
+     return product
+    })
+  }
+
+  // getters
+  const isFavoriteCount = computed(() => {
+    return productList.value.filter((product) => product.isFavorite).length
+  }) 
+
+
+  return { productList, updateProduct, isFavoriteCount }
+})
