411d6fa9cfc33e3ebc2205311fb8e8994ab1b11b
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-13 10:30:57 +0900

    vue_hw_6_2

---
+++ Practice/Vue_practice/vue_day6/vue_hw_6_2/my-third-front-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MainPage />
+    <RouterLink to="/">Some</RouterLink> |
+    <RouterLink to="/other">Other</RouterLink>
+  </div>
+  <RouterView />
+</template>
+
+<script setup>
+import MainPage from '@/components/MainPage.vue';
+import {RouterLink, RouterView} from 'vue-router'
+
+
+</script>
+
+<style scoped>
+
+</style>
