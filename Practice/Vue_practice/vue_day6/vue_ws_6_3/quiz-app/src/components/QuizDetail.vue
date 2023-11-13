5785b3432540c3d374fc5158e967df45bc230d3d
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-13 10:30:11 +0900

    vue_ws_6_3

---
+++ Practice/Vue_practice/vue_day6/vue_ws_6_3/quiz-app/src/components/QuizDetail.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="quiz-item">
+    <h4 class="quiz-question">{{ quiz.pk }}번 문제. {{ quiz.question }}</h4>
+    <label :for="'answer' + quiz.pk" class="answer-label">정답 입력</label>
+    <input type="text" :id="'answer' + quiz.pk" class="answer-input">
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  quiz : Object
+})
+
+</script>
+
+<style scoped>
+.quiz-item {
+  background-color: #f9f9f9;
+  max-width: 800px;
+  margin: 0 auto;
+  border-radius: 10px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin-top: 50px;
+}
+
+.quiz-question {
+  padding-bottom: 10px;
+  font-size: 18px;
+  margin: 0;
+}
+
+.answer-label {
+  display: block;
+  font-size: 16px;
+  margin-top: 15px;
+}
+
+.answer-input {
+  width: calc(100% - 24px);
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  margin-top: 8px;
+}
+
+</style>
---
+++ Practice/Vue_practice/vue_day6/vue_ws_6_3/quiz-app/src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main-page">
+    <h2>퀴즈 앱 사용 방법</h2>
+    <div class="usage-section">
+      <div class="usage-item">
+        <h1>1. 퀴즈 목록 확인하기</h1>
+        <p>퀴즈 목록 페이지에서 다양한 퀴즈를 확인하세요.</p>
+        <img src="@/assets/quizList.png" alt="퀴즈 목록" class="screenshot">
+      </div>
+      <div class="usage-item">
+        <h1>2. 퀴즈 생성하기 </h1>
+        <p>퀴즈는 직접 생성할 수도 있어요.</p>
+        <img src="@/assets/quizCreate.png" alt="퀴즈 생성" class="screenshot">
+      </div>
+      <div class="usage-item">
+        <h1>3. 정답 페이지</h1>
+        <p>작성한 답안이 정답인지 확인하세요.</p>
+        <img src="@/assets/quizAnswer.png" alt="정답 확인" class="screenshot">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main-page {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+
+.usage-section {
+  display: flex;
+  flex-direction: column;
+}
+
+.usage-item {
+  margin-bottom: 20px;
+}
+
+.screenshot {
+  max-width: 100%;
+  border: 1px solid #ddd;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+</style>
