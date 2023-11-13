48dcd107cd9559eb51f7c6b4a33008185c2c2ae0
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-13 17:44:11 +0900

    vue_ws_7_5

---
+++ Practice/Vue_practice/vue_day7/vue_ws_7_5/vue-project/src/main.js
@@ -0,0 +1,19 @@
+// 공식문서 보고 pinia 등록하기!
+
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
+
+// app.use(createPinia())
+app.use(router)
+app.use(pinia)
+
+app.mount('#app')
---
+++ Practice/Vue_practice/vue_day7/vue_ws_7_5/vue-project/src/views/FavoriteView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="product-list-page">
+    <h1 class="page-title">장바구니 페이지</h1>
+    <div class="product-card-list">
+      <ProductDetail 
+        v-for="product in store.isFavoriteList"
+        :key="product.name"
+        :product="product"
+      />
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import ProductDetail from '@/components/ProductDetail.vue';
+import { useProductStore } from '@/stores/product.js'
+
+const store = useProductStore()
+
+</script>
+
+<style scoped>
+.product-list-page {
+  padding: 20px;
+}
+
+.page-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.product-card-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+
+</style>
