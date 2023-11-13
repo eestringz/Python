c0030d8be990d93cdb6bfb6b3235ceda740f6863
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-13 17:43:56 +0900

    vue_ws_7_4

---
+++ Practice/Vue_practice/vue_day7/vue_ws_7_4/vue-project/src/stores/product.js
@@ -0,0 +1,54 @@
+import { ref, computed } from 'vue'
+import { defineStore  } from 'pinia'
+
+export const useProductStore = defineStore('product', () => {
+  // states
+  const productList = ref([
+    {
+      name: '상품 1',
+      imagePath: 'src/assets/product1.png',
+      price: 10000,
+      isFavorite: false
+    },
+    {
+      name: '상품 2',
+      imagePath: 'src/assets/product2.png',
+      price: 20000,
+      isFavorite: false
+    },
+    {
+      name: '상품 3',
+      imagePath: 'src/assets/product3.png',
+      price: 30000,
+      isFavorite: false
+    },
+    {
+      name: '상품 4',
+      imagePath: 'src/assets/product4.png',
+      price: 40000,
+      isFavorite: false
+    }
+  ])
+
+  // actions
+  const updateProduct = function (name) {
+    productList.value = productList.value.map((product) => {
+      if (product.name === name ) {
+        product.isFavorite = !product.isFavorite
+      }
+     return product
+    })
+  }
+
+  // getters
+  const isFavoriteCount = computed(() => {
+    return productList.value.filter((product) => product.isFavorite).length
+  }) 
+
+  const isFavoriteList = computed(() => {
+    return productList.value.filter((product) => product.isFavorite)
+  })
+
+
+  return { productList, updateProduct, isFavoriteCount, isFavoriteList }
+})
---
+++ Practice/Vue_practice/vue_day7/vue_ws_7_4/vue-project/src/views/ProductVIew.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="product-list-page">
+    <h1 class="page-title">상품 목록 페이지</h1>
+    <div class="product-card-list">
+      <ProductDetail 
+      v-for="product in store.productList"
+      :key="product.name"
+      :product="product"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProductDetail from '@/components/ProductDetail.vue';
+import { useProductStore } from '@/stores/product'
+
+const store = useProductStore()
+
+
+// console.log(product.productList)
+
+</script>
+
+<style scoped>
+.product-list-page {
+  padding: 20px;
+}
+
+.page-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.product-card-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+</style>
