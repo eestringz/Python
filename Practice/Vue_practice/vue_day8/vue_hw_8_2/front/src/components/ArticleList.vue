4e9bc70db2a729c7f1d2a97563ee5f6f69b7703b
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-14 17:26:08 +0900

    vue_hw_8_2

---
+++ Practice/Vue_practice/vue_day8/vue_hw_8_2/front/src/components/ArticleList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <ul v-for="article in store.articles" :key="article.id">
+      <li>제목 : {{ article.title }} -
+        내용 : {{article.content}}</li>
+    </ul> -->
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useArticleStore } from '@/stores/articles'
+const store = useArticleStore()
+
+onMounted(() => {
+  store.getArticles()
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
