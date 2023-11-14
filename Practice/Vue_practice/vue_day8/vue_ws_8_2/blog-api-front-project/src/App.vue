ba352634a97a0f7626fb3ee50487896287746beb
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-14 17:25:10 +0900

    vue_ws_8_2

---
+++ Practice/Vue_practice/vue_day8/vue_ws_8_2/blog-api-front-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink :to="{name : 'main'}">Home</RouterLink>
+    </nav>
+  </div>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<style scoped>
+.wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  background-color: #333;
+  padding: 1px;
+}
+
+nav a {
+  color: #fff;
+  text-decoration: none;
+  margin: 1rem;
+  font-weight: bold;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ Practice/Vue_practice/vue_day8/vue_ws_8_2/blog-api-front-project/src/views/MainView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-page">
+    <h1>My Blog!</h1>
+    <p>Welcome to my personal blog!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f7f7f7;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1rem;
+  color: #666;
+}
+</style>
